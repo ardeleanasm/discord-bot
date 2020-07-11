f70674066a3ca6e0a9e99ebc284e9d9fd5f28b17
ardeleanasm <ardeleanasm@gmail.com>
2020-07-11 12:54:54 -0400

    Implemented uptime but not working

--- src/Commands.hs
+++ src/Commands.hs
@@ -1,11 +1,13 @@
 {-# LANGUAGE OverloadedStrings #-}
 module Commands (
-    executeCommand
-
+    execute
+  , AvailableCommand(..)
+  , CommandContext(..)
     ) where
-
+import Control.Monad.IO.Class 
 import Control.Monad (when)
-import Data.Text (isPrefixOf, toLower, Text)
+import Data.Text (isPrefixOf, toLower, Text,pack)
+import Data.Time
 import Control.Concurrent (threadDelay)
 import qualified Data.Text.IO as TIO
 
@@ -13,36 +15,50 @@ import Discord
 import Discord.Types
 import qualified Discord.Requests as R
 
---executeCommand::DiscordHandle->Message -> IO ()
---executeCommand dis m = when (not (fromBot m) && isPing (messageText m)) $ do
---    _ <- restCall dis (R.CreateReaction (messageChannel m, messageId m) "eyes")
---    threadDelay (1 * 10^6)
---    _ <- restCall dis (R.CreateMessage (messageChannel m) "pong!")
---    pure ()
 
 
-executeCommand :: DiscordHandle -> Message -> IO ()
-executeCommand dis m = when (not (fromBot m) ) $ do
-    _ <- restCall dis (R.CreateReaction (messageChannel m, messageId m) "eyes")
+data AvailableCommand = PING | UPTIME | NONE deriving (Eq, Show)
+
+data CommandContext = CommandContext {
+      handler :: DiscordHandle
+    , message :: Message
+    , startTime :: UTCTime
+    } 
+
+execute :: CommandContext -> IO ()
+execute cmd = when (not (fromBot $ message cmd)) $ do
+    case (parseCommand $ messageText (message cmd)) of
+        PING -> pingResponse cmd 
+        UPTIME -> uptimeResponse cmd
+
+
+
+parseCommand :: Text -> AvailableCommand
+parseCommand receivedMessage 
+    | isPing receivedMessage    = PING
+    | isUptime receivedMessage  = UPTIME
+    | otherwise                 = NONE
+    
+
+
+-- | Commands
+uptimeResponse :: CommandContext -> IO () 
+uptimeResponse cmd= do
+    _ <- restCall (handler cmd) (R.CreateReaction (messageChannel (message cmd), messageId (message cmd)) "fire")
     threadDelay (1 * 10^6)
-    _ <- restCall dis (R.CreateMessage (messageChannel m) "pong!")
+    now <- getCurrentTime
+    zero <- return (startTime cmd)
+    _ <- restCall (handler cmd) (R.CreateMessage (messageChannel (message cmd)) (pack $ pretty (diffUTCTime now zero)))
+
     pure ()
 
-parseCommand :: Text -> IO ()
-parseCommand rxMessage = pure()
---Nothing
 
-
--- | Commands 
-uptime = Nothing
-
-bribe = Nothing
-
-befriend = Nothing
-
-bark = Nothing
-
-ping = Nothing
+pingResponse :: CommandContext -> IO ()
+pingResponse cmd = do 
+    _ <- restCall (handler cmd) (R.CreateReaction (messageChannel (message cmd), messageId (message cmd)) "eyes")
+    threadDelay (1 * 10^6)
+    _ <- restCall (handler cmd) (R.CreateMessage (messageChannel (message cmd)) "pong!")
+    pure ()
 
 
 fromBot :: Message -> Bool
@@ -50,3 +66,21 @@ fromBot m = userIsBot (messageAuthor m)
 
 isPing :: Text -> Bool
 isPing = ("ping" `isPrefixOf`) . toLower
+
+isUptime :: Text -> Bool
+isUptime = ("uptime" `isPrefixOf`) . toLower
+
+
+pretty :: NominalDiffTime -> String
+pretty diff =
+    unwords
+      . map (\(t, unit) -> show t ++ unit)
+      $ if null diffs then [(0, "s")] else diffs
+    where
+        diffs :: [(Integer, String)]
+        diffs = filter ((/= 0) . fst)
+            $ decompose [(86400, "d"), (3600, "h"), (60, "m"), (1, "s")] (floor diff)
+        decompose [] _ = []
+        decompose ((secs, unit) : metrics) t =
+            let (n, t') = t `divMod` secs
+            in (n, unit) : decompose metrics t' 
--- src/Main.hs
+++ src/Main.hs
@@ -1,6 +1,7 @@
 {-# LANGUAGE OverloadedStrings #-}  -- allows "string literals" to be Text
+{-# LANGUAGE TemplateHaskell, RankNTypes #-}
 module Main where
-import Data.Text(unpack) --TO BE REMOVED, JUST FOR DEBUG
+import Data.Text(unpack,pack) --TO BE REMOVED, JUST FOR DEBUG
 import Data.Text.Internal
 import qualified Data.Text.IO as TIO
 import Discord
@@ -8,7 +9,11 @@ import Discord.Types
 import qualified Discord.Requests as R
 import System.Environment (getArgs,getProgName)
 import System.Directory (doesFileExist)
-import EventHandlers
+import Data.Time
+import Data.Maybe
+import Control.Monad (when)
+import Control.Lens
+import Commands
 
 -- | TODO: 
 -- 1. Read token from config file
@@ -17,20 +22,52 @@ import EventHandlers
 -- 4. uptime feature
 -- 5. 
 
+data BotState = BotState
+    {
+        _botCurrentTime :: Maybe UTCTime
+    } deriving (Show)
 
--- | Replies "pong" to every message that starts with "ping"
+makeLenses ''BotState
+
+startCurrentTime::BotState
+startCurrentTime = BotState Nothing
+
 connect :: Text -> IO ()
 connect token = do 
     userFacingError <- runDiscord $ def 
             { discordToken = token
+            , discordOnStart = onDiscordStartEventHandler
             , discordOnEvent = onDiscordEventHandler 
             }
     TIO.putStrLn userFacingError
 
+onDiscordStartEventHandler :: DiscordHandle -> IO ()
+onDiscordStartEventHandler dis = do
+    currentTime <- getCurrentTime
+
+    _ <- return (set botCurrentTime (Just currentTime) startCurrentTime)
+    TIO.putStrLn $ pack ( show (view botCurrentTime startCurrentTime))
+
+
+newCurrentTime :: UTCTime -> IO BotState
+newCurrentTime t = return (BotState (Just t))
+
+onDiscordEventHandler :: DiscordHandle -> Event -> IO ()
+onDiscordEventHandler dis event = case event of
+    MessageCreate m -> execute CommandContext{handler = dis, message = m,startTime = fromJust (view botCurrentTime startCurrentTime)} 
+    _ -> pure ()
+
+parseArgument :: [String] -> IO (String)
+parseArgument args = do 
+    case args of
+        [path] -> return (path)
+        [] -> return ("")
+
 main :: IO ()
 main = do
     args <- getArgs
-    fileExists <- doesFileExist $ args !! 0 
+    path <- parseArgument args
+    fileExists <- doesFileExist path
     if fileExists == True then do
         content <- TIO.readFile path
         connect content
